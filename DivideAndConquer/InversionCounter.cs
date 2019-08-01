0b3eb01dc4c7742586f88fa1a249cc728ea5b881
carlos-chourio <carlos18daniel@gmail.com>
2019-07-31 22:32:30 -0400

    Adds unimplemented Inversion Counter

---
+++ DivideAndConquer/InversionCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DivideAndConquer {
+    enum CountingType {
+        Left,
+        Right,
+        Split
+    }
+
+    public class InversionCounter<T> where T : IComparable {
+        private int _LeftCount = 0;
+        private int _RightCount = 0;
+        private int _SplitCount = 0;
+        public int Count(T[] array) {
+            //if (array.Length == 2) {
+            //    if (array[0].CompareTo(array[1]) < 0) {
+                    
+            //    } else {
+            //        int leftCount = 
+            //    }
+            //}
+        }
+    }
+}
